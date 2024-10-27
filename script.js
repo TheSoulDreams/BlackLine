5a1ed88d703910a3cb5be3aa702ca5472c5e7974
TheSoulDreams <negativiti2@gmail.com>
2024-10-27 12:12:20 +0300

    1

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     draw() {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = 'white';
       ctx.fill();
       ctx.closePath();
     }
